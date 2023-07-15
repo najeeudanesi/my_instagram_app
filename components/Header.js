ea9f6384306a6cb3183920d441ab96d312d60ba3
najeeudanesi <najeeudanesi@gmail.com>
2023-07-15 07:24:45 +0100

    mobile nav fix

--- components/Header.js
+++ components/Header.js
@@ -64,7 +64,7 @@ export default function Header() {
               objectFit="contain"
             />
           </div>
-          <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
+          <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer" onClick={() => router.push("/")}>
             <Image
               src="https://links.papareact.com/jjm"
               layout="fill"
@@ -77,26 +77,26 @@ export default function Header() {
           <div className="max-w-xs">
             <div className="relative mt-1 p-3 rounded-md">
               <div className="absolute inset-y-0 pl-3 flex items-center  pointer-events-none">
-                <MagnifyingGlassIcon className="h-5 w-5 text-gray-500" />
+                <MagnifyingGlassIcon className="h-3 w-3 sm:text-xs lg:h-5 lg:w-5  text-gray-500" />
               </div>
 
               <input
-                className="bg-gray-50 block w-full pl-10 sm:text-sm
+                className="bg-gray-50 block  lg:w-full  pl-10 text-xs
             border-gray-300 focus:ring-black focus:border-black rounded-md"
                 type={"text"}
-                placeholder="search"
+               
               />
+
             </div>
           </div>
 
           {/*right*/}
           <div className=" flex items-center justify-end space-x-4">
             <HomeIcon onClick={() => router.push("/")} className="navBtn" />
-            <Bars4Icon className="h-6 w-6 md:hidden cursor-pointer" />
-
+           
             {user ? (
               <>
-                <div className="relative navBtn">
+                <div className="relative">
                   <PaperAirplaneIcon className="navBtn -rotate-45" />
                   <div
                     className="absolute -top-1 -right-1 text-xs w-4 bg-red-600 rounded-full text-white 
